cb7bd0db73223428ec600cdb6872b5f94653ced9
hollybelly <ri.vistorobskaya@gmail.com>
2023-01-31 22:40:39 +0300

    feat: add categories to header

--- components/layouts/headers/header1.js
+++ components/layouts/headers/header1.js
@@ -1,68 +1,106 @@
-import { Container, Grid } from "@mantine/core";
-import Image from "next/image";
+import { useState } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
+
+import {
+  useMantineTheme,
+  Header,
+  Container,
+  Group,
+  Burger,
+  Text,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import styled from "styled-components";
-import useUserParams from "../../common/userParams";
 
-const HeaderWrapper = styled.div`
-  width: 100%;
-  height: ${(props) => (props.isMobile ? "60px" : "100px")};
-  background: white;
-  border-bottom: solid 1px #d4d2d2;
-  position: ${(props) => (props.sticky ? "sticky" : "unset")};
-  top: 0;
-  z-index: 9999;
-  box-shadow: 0 2px 14px -3px #e5d1d1;
-`;
+const HEADER_HEIGHT = 150;
+const NAVBAR_HEIGHT = 50;
 
-const HeaderTitle = styled.span`
-  text-decoration: none;
-  font-size: 45px;
-  font-weight: 900;
-  color: #d6b047;
-`;
+const categories = [
+  { link: "/", label: "Home" },
+  { link: "/luxury-destinations", label: "Luxury Destinations" },
+  { link: "/fine-dining", label: "Fine Dining" },
+  { link: "/luxury-brands", label: "Luxury Brands" },
+  { link: "/luxury-in-media", label: "Luxury In Media" },
+];
 
-const HeaderSubtitle = styled.div`
-  color: #aaa;
-  font-size: 16px;
-  line-height: 24px;
-  margin-bottom: 15px;
-`;
+export default function Header1() {
+  const [active, setActive] = useState(0);
+  const [opened, { toggle }] = useDisclosure(false);
 
-const CategoriesWrapper = styled.div`
-  width: 100%;
-  height: 40px;
-  border-bottom: solid 1px #d4d2d2;
-  z-index: 10;
-  @media only screen and (max-width: 576px) {
-    display: none;
+  const links = categories.map((item, index) => (
+    <Item
+      fz="md"
+      component="a"
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(index);
+      }}
+      active={active === index}
+      px={15}
+    >
+      {item.label}
+    </Item>
+  ));
+
+  return (
+    <Header height={HEADER_HEIGHT}>
+      <Wrapper>
+        <Container>
+          <Link href="/">
+            <img
+              src="https://welivelux.com/wp-content/themes/pupabc-wordpress-template/src/img/header-logo.png"
+              alt="Home"
+            />
+          </Link>
+          <Text c="dimmed">Sharing our passion for the luxury lifestyle.</Text>
+        </Container>
+        <NavbarWrapper>
+          <Container>
+            <Navbar spacing={0}>{links}</Navbar>
+          </Container>
+        </NavbarWrapper>
+        {/* <Burger opened={opened} onClick={toggle} size="sm" /> */}
+      </Wrapper>
+    </Header>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  & > div {
+    width: 100%;
   }
 `;
 
-export default function Header1({ categories }) {
-  return (
-    <>
-      <HeaderWrapper sticky={false}>
-        <Container size={"xl"}>
-          {/* {sites[index]} */}
-          {/* <Image loader={({ src }) => src} src={'/header-logo.webp'} width={250} height={45}></Image> */}
-          <Link href="/" style={{ textDecoration: "none" }}>
-            <HeaderTitle>We Live Lux</HeaderTitle>
-          </Link>
-          <HeaderSubtitle>
-            Sharing our passion for the luxury lifestyle.
-          </HeaderSubtitle>
-        </Container>
-      </HeaderWrapper>
-      {
-        // categories &&
-        // (
-        //     <CategoriesWrapper>
-        //         <Link href='/test1?utm_source=facebook&utm_campaign=123'>sad</Link>
-        //     </CategoriesWrapper>
-        // )
-      }
-    </>
-  );
-}
+const NavbarWrapper = styled.div(() => {
+  const theme = useMantineTheme();
+
+  return `border-top: 2px solid ${theme.colors.gray[1]};`;
+});
+
+const Navbar = styled(Group)`
+  height: ${NAVBAR_HEIGHT}px;
+`;
+
+const Item = styled(Text)(({ active }) => {
+  const theme = useMantineTheme();
+
+  return `line-height: ${NAVBAR_HEIGHT - 2}px;
+  border-left: 1px solid ${theme.colors.gray[1]};
+  border-bottom: 2px solid transparent;
+  color: ${theme.colors.dark[5]};
+  transition: box-shadow 300ms ease, color 300ms ease;
+  ${active && "box-shadow: 0 2px 2px rgb(0 0 0 / 10%);"}
+  &:last-child {
+    border-right: 1px solid ${theme.colors.gray[1]};
+  }
+  &:hover {
+    color: ${theme.primaryColor};
+    box-shadow: 0 2px 2px rgb(0 0 0 / 10%);
+  }`;
+});
