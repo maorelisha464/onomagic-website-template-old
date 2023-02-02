61dae81aa9c6e36403e27b79895c4b301ae892b9
Maor Elisha <maorelisha464@gmail.com>
2023-02-02 17:21:09 +0200

    NEXT_PUBLIC_EXCLUDE_CATEGORY

--- components/common/datafetching/dataFetcher.js
+++ components/common/datafetching/dataFetcher.js
@@ -1,7 +1,7 @@
 import { getCategoryId, getPostAmount, mapPosts } from "./utils";
 import { getPostsWithCategory, getCategories } from "./dataFetcherRequests";
 
-const categoryToExcludeDefault = process.env.EXCLUDE_CATEGORY || "celebrity-luxury";
+const categoryToExcludeDefault = process.env.NEXT_PUBLIC_EXCLUDE_CATEGORY || "celebrity-luxury";
 
 export const getPostsWithCategories = async (specificCat, page = 1, slug = categoryToExcludeDefault) => {
   const categories = await (await getCategories()).json();
